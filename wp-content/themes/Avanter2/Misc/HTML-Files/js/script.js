933b31f73f9b0b7e4ee3f12670efcd6167ed9b81
Narciso Avalos <narciso@masfusions-iMac-5.local>
2014-05-29 16:40:56 -0500

    WP Init Load

---
+++ wp-content/themes/Avanter2/Misc/HTML-Files/js/script.js
@@ -0,0 +1,82 @@
+/*  Table of Contents 
+01. MENU ACTIVATION
+04. prettyPhoto Activation
+05. FITVIDES RESPONSIVE VIDEOS
+07. MOBILE SELECT MENU
+08. IE7 z-index fix
+*/
+
+
+/*
+=============================================== 01. MENU ACTIVATION  ===============================================
+*/
+
+
+jQuery(document).ready(function() {
+		jQuery('ul.sf-menu').superfish({
+			delay:       200,                            // one second delay on mouseout
+			animation:   {opacity:'show',height:'show'},  // fade-in and slide-down animation
+			speed:       'fast',                          // faster animation speed
+			autoArrows:  false,                            // disable generation of arrow mark-up
+			cssArrows: false  //disable generation of arrow mark-up
+		});
+});
+
+
+
+/*
+=============================================== 04. prettyPhoto Activation  ===============================================
+*/
+jQuery(document).ready(function($) {
+		jQuery("a[rel^='prettyPhoto']").prettyPhoto({
+			animation_speed: 'fast', /* fast/slow/normal */
+			slideshow: 5000, /* false OR interval time in ms */
+			autoplay_slideshow: false, /* true/false */
+			opacity: 0.80, /* Value between 0 and 1 */
+			show_title: false, /* true/false */
+			allow_resize: true, /* Resize the photos bigger than viewport. true/false */
+			default_width: 500,
+			default_height: 344,
+			counter_separator_label: '/', /* The separator for the gallery counter 1 "of" 2 */
+			theme: 'pp_default', /* light_rounded / dark_rounded / light_square / dark_square / facebook */
+			horizontal_padding: 20, /* The padding on each side of the picture */
+			hideflash: false, /* Hides all the flash object on a page, set to TRUE if flash appears over prettyPhoto */
+			wmode: 'opaque', /* Set the flash wmode attribute */
+			autoplay: false, /* Automatically start videos: True/False */
+			modal: false, /* If set to true, only the close button will close the window */
+			deeplinking: false, /* Allow prettyPhoto to update the url to enable deeplinking. */
+			overlay_gallery: false, /* If set to true, a gallery will overlay the fullscreen image on mouse over */
+			keyboard_shortcuts: true, /* Set to false if you open forms inside prettyPhoto */
+			ie6_fallback: true,
+			social_tools: '' /* html or false to disable  <div class="pp_social"><div class="twitter"><a href="http://twitter.com/share" class="twitter-share-button" data-count="none">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script></div><div class="facebook"><iframe src="http://www.facebook.com/plugins/like.php?locale=en_US&href='+location.href+'&amp;layout=button_count&amp;show_faces=true&amp;width=500&amp;action=like&amp;font&amp;colorscheme=light&amp;height=23" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:500px; height:23px;" allowTransparency="true"></iframe></div></div> */
+		});
+});
+
+
+/*
+=============================================== 05. FITVIDES RESPONSIVE VIDEOS  ===============================================
+*/
+jQuery(document).ready(function($) {  
+$("#main").fitVids();
+$(".flexslider").fitVids();
+});
+
+
+/*
+=============================================== 07. MOBILE SELECT MENU  ===============================================
+*/
+
+jQuery(document).ready(function($) {
+$('.sf-menu').mobileMenu({
+    defaultText: 'Navigate to...',
+    className: 'select-menu',
+    subMenuDash: '&ndash;&ndash;'
+});
+});
+
+
+
+
+
+
+
